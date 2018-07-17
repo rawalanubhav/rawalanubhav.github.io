9b5c3c825ad7d6dca096f14424a3643900f73c7a
rawalanubhav <detroid.stan@gmail.com>
2018-07-17 08:50:40 +0545

    Redesiging the path locations

--- ext/main.js
+++ ext/main.js
@@ -3,4 +3,11 @@ $(".navbar a").click(function(){
    scrollTop:$("#" + $(this).data('value')).offset().top
   },1000)
   
- })
+ });
+
+$(function() {
+ $('[data-toggle="datepicker"]').datepicker({
+   autoHide: true,
+   zIndex: 2048,
+ });
+});
